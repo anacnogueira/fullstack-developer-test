f6d81836ae5e093f83d88312546653aa72a0f421
Ana Claudia Nogueira <anaclaudia@mayacosmeticos.com.br>
2021-06-10 15:08:41 -0300

    Adiciona Bootstrap como modulo

--- spa/src/main.ts
+++ spa/src/main.ts
@@ -5,6 +5,8 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 axios.defaults.baseURL = 'http://guide-121-api.test/api/'
 
 createApp(App).use(VueAxios, axios).use(store).use(router).mount('#app')
