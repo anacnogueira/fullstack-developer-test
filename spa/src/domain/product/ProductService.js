197ce240c90d5267140578db94d6fbe57eb4f9a3
Ana Claudia Nogueira <anaclaudia@mayacosmeticos.com.br>
2021-06-10 16:22:31 -0300

    Trata erros de retorno da api em alto nivel na SPA

--- spa/src/domain/product/ProductService.js
+++ spa/src/domain/product/ProductService.js
@@ -12,31 +12,48 @@ export default class ProductService {
       })
       .catch(error => {
         console.log(error)
-        throw new Error('Não Foi Possível carregar os produtos. Tente mais tarde')
+        throw new Error('Não foi possível carregar os produtos. Tente mais tarde')
       })
   }
 
-  // TODO: Tratar erro
   store (product) {
     if (product.id) {
       return this._resource
         .put(`${this._endpoint}/${product.id}`, product)
+        .then(() => null)
+        .catch(error => {
+          console.log(error)
+          throw new Error('Não foi possível alterar o produto.')
+        })
     }
 
     return this._resource
       .post(this._endpoint, product)
+      .then(() => null)
+      .catch(error => {
+        console.log(error)
+        throw new Error('Não foi possível cadastrar o produto.')
+      })
   }
 
-  // TODO: Tratar erro
   show (id) {
     return this._resource
       .get(`${this._endpoint}/${id}`)
       .then(response => response.data.product)
+      .catch(error => {
+        console.log(error)
+        throw new Error('Não foi carregar o produto.')
+      })
   }
 
   // TODO: Tratar erro
   delete (id) {
     return this._resource
       .delete(`${this._endpoint}/${id}`)
+      .then(() => null)
+      .catch(error => {
+        console.log(error)
+        throw new Error('Não foi excluir o produto.')
+      })
   }
 }
